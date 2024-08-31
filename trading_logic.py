b5ce10bd71ab3e5624b4ac2f372887ca9dd0c658
matthewd-so <mattut319@gmail.com>
2024-08-31 10:24:17 -0400

    More Strategies

--- trading_logic.py
+++ trading_logic.py
@@ -13,8 +13,81 @@ def moving_average_strategy(data, short_window=20, long_window=50):
 
     return data, signals_df
 
+# Relative Strength Index
+def rsi_strategy(data, window=14):
+    delta = data['close'].diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
+    rs = gain / loss
+    rsi = 100 - (100 / (1 + rs))
+    data['rsi'] = rsi
 
-    return data
+    data['signal'] = 0
+    data.loc[data['rsi'] < 30, 'signal'] = 1  # Buy signal
+    data.loc[data['rsi'] > 70, 'signal'] = -1  # Sell signal
+
+    signals_df = data[data['signal'] != 0][['signal']]
+
+    return data, signals_df
+
+# MACD
+def macd_strategy(data, fast_window=12, slow_window=26, signal_window=9):
+    data['ema_fast'] = data['close'].ewm(span=fast_window, min_periods=slow_window).mean()
+    data['ema_slow'] = data['close'].ewm(span=slow_window, min_periods=slow_window).mean()
+    data['macd'] = data['ema_fast'] - data['ema_slow']
+    data['macd_signal'] = data['macd'].ewm(span=signal_window, min_periods=signal_window).mean()
+
+    data['signal'] = 0
+    data.loc[data['macd'] > data['macd_signal'], 'signal'] = 1  # Buy signal
+    data.loc[data['macd'] < data['macd_signal'], 'signal'] = -1  # Sell signal
+
+    signals_df = data[data['signal'] != 0][['signal']]
+
+    return data, signals_df
+
+# Bollinger Bands
+def bollinger_bands_strategy(data, window=20, num_std_dev=2):
+    data['middle_band'] = data['close'].rolling(window=window).mean()
+    data['upper_band'] = data['middle_band'] + num_std_dev * data['close'].rolling(window=window).std()
+    data['lower_band'] = data['middle_band'] - num_std_dev * data['close'].rolling(window=window).std()
+
+    data['signal'] = 0
+    data.loc[data['close'] < data['lower_band'], 'signal'] = 1  # Buy signal
+    data.loc[data['close'] > data['upper_band'], 'signal'] = -1  # Sell signal
+
+    signals_df = data[data['signal'] != 0][['signal']]
+
+    return data, signals_df
+
+# Stochastic Oscillator
+def stochastic_oscillator_strategy(data, k_window=14, d_window=3):
+    data['low_k'] = data['low'].rolling(window=k_window).min()
+    data['high_k'] = data['high'].rolling(window=k_window).max()
+    data['%K'] = (data['close'] - data['low_k']) / (data['high_k'] - data['low_k']) * 100
+    data['%D'] = data['%K'].rolling(window=d_window).mean()
+
+    data['signal'] = 0
+    data.loc[(data['%K'] < 20) & (data['%D'] < 20), 'signal'] = 1  # Buy signal
+    data.loc[(data['%K'] > 80) & (data['%D'] > 80), 'signal'] = -1  # Sell signal
+
+    signals_df = data[data['signal'] != 0][['signal']]
+
+    return data, signals_df
+
+# SMA Crossover
+def sma_crossover_strategy(data, short_window=50, long_window=200):
+    data['short_sma'] = data['close'].rolling(window=short_window).mean()
+    data['long_sma'] = data['close'].rolling(window=long_window).mean()
+
+    data['signal'] = 0
+    data.loc[data['short_sma'] > data['long_sma'], 'signal'] = 1  # Buy signal
+    data.loc[data['short_sma'] < data['long_sma'], 'signal'] = -1  # Sell signal
+
+    signals_df = data[data['signal'] != 0][['signal']]
+
+    return data, signals_df
+
+
 def simulate_trading(data, initial_capital):
     position = 0  # 0 means no position, 1 means we have bought
     balance = initial_capital
